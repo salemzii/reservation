62e9a37cfe666dfa9bfa657c41907d7c9011fc75
salemzii <salemododa2@gmail.com>
2022-05-16 16:31:15 +0100

    template

--- reservations/views.py
+++ reservations/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from .models import Rental, Reservations
+from django.views import View
 
 
 def All_Reservation(request):
 
+    # create sublist of reservation details for all reservation records
     reservations = [[res.rental_id.name, res.id, res.checkin, res.checkout] for res in Reservations.objects.all()]
     rental_names = Rental.objects.values_list('name')
 
     ValueSet = []
 
+    # get res-id or - for each reservation using it's rental name
     for i in sort_by_rental_name(rental_names):
         ValueSet += i
 
@@ -22,6 +25,14 @@ def All_Reservation(request):
 
 
 def sort_by_rental_name(r:any) -> list:
+
+    '''
+        filter and define value for each reservation record, in reservation table
+        using the rental_name
+
+        this function is a generator that returns a values list
+        i.e  ("-" or the previous Reservation id) of the record before.
+    '''
 
     for i in r:
         Qset = Reservations.objects.filter(rental_id__name=i[0])
